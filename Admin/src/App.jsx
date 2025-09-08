d8e73340be3bbc1c273d482653a81dee00e2318f
Sadid Alam <sadidalam14@gmail.com>
2025-09-08 13:31:06 +0530

    tailwind setup and added custom color, font

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex flex-col h-screen w-full bg-background">
+      <h1 className="text-text-heading font-semibold font-heading text-5xl text-center">
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <div className="w-18 h-12 bg-accent rounded-full">
+        <button className="px-5 py-2 w-full h-full">Click</button>
+      </div>
+      <p className="text-3xl text-text-subtle font-body text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        sunt incidunt quas aspernatur et.
+      </p>
+    </div>
+  );
 };
 
 export default App;
