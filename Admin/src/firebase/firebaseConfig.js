5b15e24d34c95101e804453b36328fb914f75d5c
Sadid Alam <sadidalam14@gmail.com>
2025-11-19 17:21:31 +0530

    added recaptcha key in firebase config

--- Admin/src/firebase/firebaseConfig.js
+++ Admin/src/firebase/firebaseConfig.js
@@ -4,6 +4,9 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
 
+// 1. Import App Check libraries
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA3VkTfSdOfJhX-MuDljst5G8npc3WQe7Q",
   authDomain: "gyanbrix-25.firebaseapp.com",
@@ -15,6 +18,25 @@ const firebaseConfig = {
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
+
+// 2. Initialize App Check
+// ============================================================
+if (typeof window !== "undefined") {
+  // FORCE the debug token using the string you just copied
+  // (Remove this line when deploying to production!)
+  if (location.hostname === "localhost") {
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = "7AB48A40-2CBD-42C4-BD58-738CA14765EA";
+  }
+
+  initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaV3Provider(
+      "6LfLwhEsAAAAAJhDuqv0pHzbaC4VccnBaCcWy0zf"
+    ),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
+// ============================================================
+
 export const firebaseAuth = getAuth(firebaseApp);
 export const firestoreDB = getFirestore(firebaseApp);
 export const firebaseStorage = getStorage(firebaseApp);
